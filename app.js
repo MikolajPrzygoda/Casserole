f33ea84cc25883fe32c8924e26023ca6a5243477
Mikołaj Przygoda <przygoda.mikolaj@gmail.com>
2018-03-22 10:51:46 +0100

    Setup basic express routing with static resources

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
 
-const http = require('http');
+const http = require('http')
+const path = require('path')
+var express = require('express')
+var app = express()
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = 20000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+// exemplary express endpoint
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'))
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+//static route
+app.use('/static', express.static(path.join(__dirname, 'static')))
+
+//error handling
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+//404 handling
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
+
+app.listen(port);
